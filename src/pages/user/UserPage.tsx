2b4fda5aff819fd6cc36dbc5ce7eb102dc640675
Oleksandr <102832044+Oleksandr073@users.noreply.github.com>
2024-05-25 19:33:54 +0300

    add logOut button to UserPage

--- src/pages/user/UserPage.tsx
+++ src/pages/user/UserPage.tsx
@@ -1,14 +1,29 @@
 import { UserInformation } from '../../components/blocks';
+import { Button } from '../../components/ui';
 import { authSelectors } from '../../redux/auth/selectors';
-import { useAppSelector } from '../../redux/hooks';
+import { authSlice } from '../../redux/auth/slice';
+import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 
 export const UserPage = () => {
+  const dispatch = useAppDispatch();
+
   const userInfo = useAppSelector(authSelectors.selectUserInfo);
+
+  const handleLogOut = () => {
+    dispatch(authSlice.actions.logOut());
+  };
+
   return (
     <div className="container py-5">
       <h2 className="font-bold text-2xl mb-4">User Information</h2>
 
-      {userInfo && <UserInformation userInfo={userInfo} />}
+      {userInfo && (
+        <div className="mb-4">
+          <UserInformation userInfo={userInfo} />
+        </div>
+      )}
+
+      <Button text="Log Out" onClick={handleLogOut} />
     </div>
   );
 };
