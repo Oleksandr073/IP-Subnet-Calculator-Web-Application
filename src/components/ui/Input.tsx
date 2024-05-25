5b5a3621fee6b3b326aad746ba2f779c2933f386
Oleksandr <102832044+Oleksandr073@users.noreply.github.com>
2024-05-25 18:56:59 +0300

    minor fix

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -8,7 +8,7 @@ export const Input = ({ value, onChange, name, label }: Props) => {
   return (
     <div>
       <label>
-        <span className="block font-medium">{label}</span>
+        {label && <span className="block font-medium">{label}</span>}
         <input
           className="rounded-sm border border-gray-400 px-1 focus:border-gray-900 focus:outline-none"
           name={name}
