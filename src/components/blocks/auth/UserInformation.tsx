d813a760d858f2fa0ab83e799491ebbab20701de
Oleksandr <102832044+Oleksandr073@users.noreply.github.com>
2024-05-25 19:26:04 +0300

    update styles in UserInformation

--- src/components/blocks/auth/UserInformation.tsx
+++ src/components/blocks/auth/UserInformation.tsx
@@ -7,7 +7,7 @@ export const UserInformation = ({ userInfo }: Props) => {
   const { displayName, email, phoneNumber, photoURL } = userInfo;
   return (
     <div>
-      {photoURL && <img className="w-20 h-20 mb-2" src={photoURL} />}
+      {photoURL && <img className="w-20 h-20 mb-2 rounded-md" src={photoURL} />}
       <p className="mb-1">
         <span className="font-medium">Name:</span> {displayName}
       </p>
