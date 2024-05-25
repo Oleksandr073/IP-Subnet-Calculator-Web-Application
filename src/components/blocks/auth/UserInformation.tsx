04a348a704e13d595e09c08850a118e6e8d3b241
Oleksandr <102832044+Oleksandr073@users.noreply.github.com>
2024-05-25 16:56:00 +0300

    add UserInformation block

---
+++ src/components/blocks/auth/UserInformation.tsx
@@ -0,0 +1,22 @@
+import { UserInfo } from 'firebase/auth';
+
+type Props = {
+  userInfo: UserInfo;
+};
+export const UserInformation = ({ userInfo }: Props) => {
+  const { displayName, email, phoneNumber, photoURL } = userInfo;
+  return (
+    <div>
+      {photoURL && <img className="w-20 h-20 mb-2" src={photoURL} />}
+      <p className="mb-1">
+        <span className="font-medium">Name:</span> {displayName}
+      </p>
+      <p className="mb-1">
+        <span className="font-medium">Email:</span> {email}
+      </p>
+      <p>
+        <span className="font-medium">PhoneNumber:</span> {phoneNumber ?? '-'}
+      </p>
+    </div>
+  );
+};
--- src/pages/user/UserPage.tsx
+++ src/pages/user/UserPage.tsx
@@ -1,7 +1,14 @@
+import { UserInformation } from '../../components/blocks';
+import { authSelectors } from '../../redux/auth/selectors';
+import { useAppSelector } from '../../redux/hooks';
+
 export const UserPage = () => {
+  const userInfo = useAppSelector(authSelectors.selectUserInfo);
   return (
     <div className="container py-5">
       <h2 className="font-bold text-2xl mb-4">User Information</h2>
+
+      {userInfo && <UserInformation userInfo={userInfo} />}
     </div>
   );
 };
